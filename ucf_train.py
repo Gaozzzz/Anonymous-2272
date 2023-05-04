935b2d4cc158dd6a23bf1336e59597506fbd32e8
Jianzhe Gao <37926649+Gaozzzz@users.noreply.github.com>
2023-05-04 14:29:53 +0800

    Update ucf_train.py

--- ucf_train.py
+++ ucf_train.py
@@ -11,7 +11,7 @@ from model.model import FadNet
 
 def parse_args():
     parser = argparse.ArgumentParser(description='FadNet gogogo!')
-    parser.add_argument('--num_iters', type=int, default=1500)
+    parser.add_argument('--num_iters', type=int, default=1000)
     parser.add_argument('--len_feature', type=int, default=1024)
     parser.add_argument('--memory_block_number', type=int, default=50)
     parser.add_argument('--batch_size', type=int, default=64)
